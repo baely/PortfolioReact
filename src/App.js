207aaf98ce70c5da90e59661a74e008c73bb4597
baely <bailey_fun@live.com.au>
2021-08-28 19:01:39 +1000

    First draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import * as Icons from './images/tech/images';
+
+
+
+const cardStackImages = {
+    "py": Icons.iconPy,
+    "ng": Icons.iconNg,
+    "aws": Icons.iconAws,
+    "lambda": Icons.iconLambda,
+    "az": Icons.iconAz,
+    "django": Icons.iconDjango,
+    "gcp": Icons.iconGcp,
+    "flask": Icons.iconFlask,
+    "java": Icons.iconJava,
+    ".net": Icons.iconCSharp,
+    "react": Icons.iconReact,
+    "psql": Icons.iconPsql,
+    "azure-function": Icons.iconAzureFunction,
+    "cosmos": Icons.iconCosmosDb
+}
+
+
+function CardStack(props) {
+    console.log(props);
+
+    const label = (props.name in cardStackImages) ? (
+        <img src={cardStackImages[props.name]} />
+    ) : (
+        <span>{props.name}</span>
+    );
+
+    return (
+        <div className="stack-item">
+            { label }
+        </div>
+    );
+}
+
+
+function Card(props) {
+    console.log(props.data);
+  return (
+      <div className="project"><a href={props.data.url}>
+          {/*<div className="display"><img src={props.data.img} alt="Project screenshot"/></div>*/}
+          <label>Title</label><div className="title">{props.data.name}</div>
+          <label>Description</label><div className="description">{props.data.description}</div>
+          <label>Stack</label><div className="stack">
+              {Object.keys(props.data.stack).map(k => <CardStack name={k} data={props.data.stack[k]} />)}
+          </div>
+      </a></div>
+  );
+}
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("https://baileyportfolio.azurewebsites.net/api/GetProjects")
+        .then(res => res.json())
+        .then(data => {
+            setProjects(data.map(proj => <Card data={proj}/>));
+        });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+          <div className="header">
+              <h1>baely.co</h1>
+          </div>
+          <div className="project-list">
+              { projects }
+          </div>
+      </main>
+
   );
 }
 
---
+++ src/images/tech/images.js
@@ -0,0 +1,33 @@
+import iconPy from './python.svg';
+import iconNg from './angular-icon.svg';
+import iconAws from './aws.svg';
+import iconLambda from './aws-lambda.svg';
+import iconAz from './azure-icon.svg';
+import iconDjango from './django-icon.svg';
+import iconGcp from './google-cloud.svg';
+import iconFlask from './flask.svg';
+import iconJava from './java.svg';
+import iconJs from './javascript.svg';
+import iconReact from './react.svg';
+import iconCSharp from './c-sharp.svg';
+import iconPsql from './postgresql.svg';
+import iconAzureFunction from './Function-Apps.svg';
+import iconCosmosDb from './Azure-Cosmos-DB.svg';
+
+export {
+    iconPy,
+    iconNg,
+    iconAws,
+    iconLambda,
+    iconAz,
+    iconDjango,
+    iconGcp,
+    iconFlask,
+    iconJava,
+    iconReact,
+    iconJs,
+    iconCSharp,
+    iconPsql,
+    iconAzureFunction,
+    iconCosmosDb
+}
